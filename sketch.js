abf962470095f7131fd6eb13faf36484ec15936f
vleeahnn <vleeahnn@pdx.edu>
2021-03-17 19:32:36 -0700

    more

--- sketch.js
+++ sketch.js
@@ -1,26 +1,27 @@
 var canvas;
-const sizeA= 30;
+const sizeA= 40;
 const sizeB= 5;
 
-function windowResize(){
- resizeCanvas(windowWidth, windowHeight);
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
 }
 function setup () {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
   imageMode(CENTER)
   canvas.position(0, 0);
-  canvas.style('z-index', '-1');
-  frameRate(3);
+  canvas.style('z-index', '-999');
+  frameRate(10);
 }
 function draw () {
   noStroke();
   rect (random(width), random(height), random (sizeA), random (sizeA));
-  fill (0, 72, 255);
+  fill (0, 72, 255, 100);
   rect (random(width), random(height), random (sizeA), random (sizeA));
-  fill (0, 72, 255);
+  fill (0, 72, 255, random(255));
   ellipse (random(width), random(height), random (sizeA), random (sizeA));
-  fill (0, 72, 255);
+  fill (0, 72, 255, random(255));
   rect (random(width), random(height), sizeB, sizeB);
-  fill (0, 72, 255);
+  fill (0, 72, 255, random(255));
   ellipse (random(width), random(height), sizeB, sizeB);
 }  
